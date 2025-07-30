4589afe4e3b403afa01c002786e7ea46c2541666
Ada-23kumar <adarsh754103@gmail.com>
2025-07-30 14:12:15 +0530

    Majority element solution in python

---
+++ Python/array/169_MajorityElement.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums:List[int]) -> int:
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        return candidate
+        
+
+
+nums = [2,2,1,1,1,2,2]
+MajEle = Solution()
+print(MajEle.majorityElement(nums))
